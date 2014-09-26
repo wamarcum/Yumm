507ab91f04d04d8c925ace9111fd239225ebda53
jakenotjacob <jake.campbell91@gmail.com>
2014-09-25 21:39:38 -0600

    Add factorygirl to Gemfile.  Implement table factory.

--- yumm/spec/features/creating_tables_spec.rb
+++ yumm/spec/features/creating_tables_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 feature 'Creating Tables' do
+  let!(:table) { FactoryGirl.create(:table) }
   scenario 'can create a table' do
     visit '/'
 
     click_link 'New Table'
-    fill_in 'Number', with: "1"
-    fill_in 'Guests', with: "2"
+    fill_in 'Number', with: "0"
+    fill_in 'Guests', with: "9"
     click_button 'Create Table'
 
     expect(page).to have_content('Table has been seated!')
 
-    table = Table.where(number: "1").first
+    table = Table.where(number: "0").first
     expect(page.current_url).to eql(table_url(table))
-    title = "Yumm - Tables - 1"
+    title = "Yumm - Tables - 0"
     expect(page).to have_title(title)
   end
 end
