4089855531acf10adff986043fafb73b8e222768
jakenotjacob <jake.campbell91@gmail.com>
2014-09-25 21:28:01 -0600

    Add title functionality for show page.

--- yumm/app/helpers/application_helper.rb
+++ yumm/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts.unshift "Yumm").join(" - ")
+      end
+    end
+  end
+
 end
--- yumm/spec/features/creating_tables_spec.rb
+++ yumm/spec/features/creating_tables_spec.rb
@@ -5,10 +5,15 @@ feature 'Creating Tables' do
     visit '/'
 
     click_link 'New Table'
-    fill_in 'Number', with: 9
-    fill_in 'Guests', with: 2
+    fill_in 'Number', with: "1"
+    fill_in 'Guests', with: "2"
     click_button 'Create Table'
 
     expect(page).to have_content('Table has been seated!')
+
+    table = Table.where(number: "1").first
+    expect(page.current_url).to eql(table_url(table))
+    title = "Yumm - Tables - 1"
+    expect(page).to have_title(title)
   end
 end
