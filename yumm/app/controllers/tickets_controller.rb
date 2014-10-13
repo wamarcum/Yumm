715afbf6bbd81359921c5ae62b4ad2eff62e1053
jakenotjacob <jake.campbell91@gmail.com>
2014-10-13 01:46:39 -0600

    Implement editing Tickets.

--- yumm/app/controllers/tickets_controller.rb
+++ yumm/app/controllers/tickets_controller.rb
@@ -17,7 +17,19 @@ class TicketsController < ApplicationController
   end
 
   def show
-    
+  end
+
+  def edit
+  end
+
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@table, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render "edit"
+    end
   end
 
   private
