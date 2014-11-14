111032b722c0194d06b8a669c37d212c7c30e606
jakenotjacob <jake.campbell91@gmail.com>
2014-11-14 00:00:02 -0700

    Make Table numbers unique.

--- yumm/db/schema.rb
+++ yumm/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021092130) do
+ActiveRecord::Schema.define(version: 20141114065122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20141021092130) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "tables", ["number"], name: "index_tables_on_number", unique: true, using: :btree
 
   create_table "tickets", force: true do |t|
     t.string   "title"
